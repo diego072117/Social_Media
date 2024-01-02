cd5122beb13c8f2f9f012309c622718ea74d8573
diego072117 <parracalderond9@gmail.com>
2024-01-02 15:45:20 -0500

    feat(listPost): the posts list has been made, working on post functionalities.

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -190,3 +190,15 @@ export async function deleteFile(fileId: string) {
     }
 
 }
+
+export async function getRecentPosts() {
+    const post = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        [Query.orderDesc('$createdAt'), Query.limit(20)]
+    )
+
+    if (!post) throw Error
+
+    return post
+}
--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -5,7 +5,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from "@tanstack/react-query"
-import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api"
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api"
 import { QUERY_KEYS } from "./queryKeys"
 
 export const useCreateUserAccount = () => {
@@ -40,3 +40,10 @@ export const useCreatePost = () => {
         },
     });
 };
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts
+    })
+}
